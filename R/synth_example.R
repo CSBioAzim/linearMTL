342069ef0b7be8b8869f26fdacf1bffe6ba9b77b
tohein <tohein@stud.uni-saarland.de>
2017-10-17 14:11:30 +0200

    added test function

---
+++ R/synth_example.R
@@ -0,0 +1,117 @@
+#' Test of tree-guided group lasso on synthetic data.
+#'
+#' Generate synthetic data as in Kim et al. 2010 and apply tree-guided group
+#' lasso and lasso models.
+#'
+#' @importFrom lattice levelplot
+#' @export
+TestLinearMTL <- function() {
+  set.seed(12345)
+  # number of data points
+  N <- 150
+  # number of input dimensions
+  J <- 200
+  # number of tasks
+  K <- 60
+  # signal intensity
+  signal <- 0.6
+
+  # generate hierarchical pattern in B.truth
+  setsignal <- function(start, thickness, steps, B.truth) {
+    d <- ncol(B.truth)
+    l <- d/steps
+    offset <- start
+    for (step in 0:(steps-1)) {
+      for (i in 1:thickness) {
+        B.truth[offset, (step * l + 1):((step + 1) * l)] <- signal
+        offset <- offset + 1
+      }
+    }
+    return(B.truth)
+  }
+  B.truth <- matrix(0, nrow = J, ncol = K)
+  B.truth <- setsignal(start = 5, thickness = 1, steps = 3, B.truth)
+  B.truth <- setsignal(start = 10, thickness = 2, steps = 6, B.truth)
+  B.truth <- setsignal(start = 30, thickness = 3, steps = 12, B.truth)
+  B.truth <- setsignal(start = 75, thickness = 1, steps = 60, B.truth)
+  B.truth <- B.truth[, ncol(B.truth):1]
+  B.truth[2, ] <- signal
+
+  # generate training and test matrices
+  Xtrain <- matrix(sample(c(0,1,2), N * J, replace = T), nrow = N)
+  Xtest <- matrix(sample(c(0,1,2), N * J, replace = T), nrow = N)
+  Ytrain <- Xtrain %*% B.truth + matrix(rnorm(N * K), nrow = N)
+  Ytest <- Xtest %*% B.truth + matrix(rnorm(N * K), nrow = N)
+
+  # generated centered versions
+  center.X <- colMeans(Xtrain)
+  Xtrain.centered <- sweep(Xtrain, 2, center.X, check.margin = TRUE)
+  Xtest.centered <- sweep(Xtest, 2, center.X, check.margin = TRUE)
+  center.Y <- colMeans(Ytrain)
+  Ytrain.centered <- sweep(Ytrain, 2, center.Y, check.margin = TRUE)
+  Ytest.centered <- sweep(Ytest, 2, center.Y, check.margin = TRUE)
+
+  ################################################################################
+  # GROUP MODEL
+  ################################################################################
+  M <- BuildTreeHC(Ytrain, 0.6)
+  lambda.vec = 10^seq(-2,2, length.out = 20)
+  # tggl <- RunGroupCrossvalidation(X = Xtrain.centered,
+  #                                 Y = Ytrain.centered,
+  #                                 groups = M$groups,
+  #                                 weights.matrix = matrix(M$weights, nrow = 1),
+  #                                 lambda.vec = lambda.vec,
+  #                                 max.iter = 10000,
+  #                                 epsilon = 1e-5,
+  #                                 mu = 0.001, mu.adapt = 0.99,
+  #                                 verbose = 1)
+  # group.B <- tggl$full.model$B
+  tggl <- TreeGuidedGroupLasso(X = Xtrain.centered,
+                               Y = Ytrain.centered,
+                               groups = M$groups,
+                               weights = M$weights,
+                               lambda = 20,
+                               max.iter = 10000,
+                               epsilon = 1e-5,
+                               mu = 0.001, mu.adapt = 0.99,
+                               verbose = 1)
+  group.B <- tggl$B
+  ################################################################################
+  # SBS MODEL
+  ################################################################################
+  lambda.vec = 10^seq(-5,2, length.out = 20)
+  tbt <- RunTBTCrossvalidation(X = Xtrain.centered,
+                               Y = Ytrain.centered,
+                               lambda.vec = lambda.vec)
+  sbs.B <- tbt$B
+  ################################################################################
+  # EVALUATION
+  ################################################################################
+  # compute test error and test correlation
+  group.test.pred <- MTPredict(B = group.B, X = Xtest.centered)
+  group.err <- MTComputeError(Y = Ytest.centered, B = group.B, X = Xtest.centered)
+  group.cor <- MTComputeMeanCorrelation(Y = Ytest.centered, B = group.B, X = Xtest.centered)
+  print(sprintf("GROUP  - test mse: %f, test cor: %f", group.err, group.cor))
+
+  sbs.test.pred <- MTPredict(B = sbs.B, X = Xtest.centered)
+  sbs.err <- MTComputeError(Y = Ytest.centered, B = sbs.B, X = Xtest.centered)
+  sbs.cor <- MTComputeMeanCorrelation(Y = Ytest.centered, B = sbs.B, X = Xtest.centered)
+  print(sprintf("SBS    - test mse: %f, test cor: %f", sbs.err, sbs.cor))
+
+  opt.pred <- MTPredict(B = B.truth, X = Xtest.centered)
+  opt.err <- MTComputeError(Y = Ytest.centered, B = B.truth, X = Xtest.centered)
+  opt.cor <- MTComputeMeanCorrelation(Y = Ytest.centered, B = B.truth, X = Xtest.centered)
+  print(sprintf("OPT    - test mse: %f, test cor: %f", opt.err, opt.cor))
+
+  # plot coefficients
+  scl <- seq(-0.3,0.5, by = 0.01)
+  colreg <- gray(1:(length(scl)+1)/(length(scl)+1))
+  lattice::levelplot(group.B, at = scl, col.regions = colreg)
+  lattice::levelplot(sbs.B, at = scl, col.regions = colreg)
+  lattice::levelplot(B.truth, at = scl, col.regions = colreg)
+
+  # plot error
+  plot(colSums((sbs.test.pred - Ytest.centered)^2) / N, col = "red", type = "p")
+  lines(colSums((group.test.pred - Ytest.centered)^2) / N, type = "p", col = "blue")
+  lines(colSums((Xtest.centered %*% B.truth - Ytest.centered)^2) / N, type = "p", col = "black")
+}
