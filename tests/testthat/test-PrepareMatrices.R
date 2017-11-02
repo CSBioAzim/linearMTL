cf4fa42d477c8bf7e50c02ae40325b6c4bcc4a91
tohein <tohein@stud.uni-saarland.de>
2017-11-02 11:09:13 +0100

    updated documentation and tests for PrepareMatrices

--- tests/testthat/test-PrepareMatrices.R
+++ tests/testthat/test-PrepareMatrices.R
@@ -4,7 +4,7 @@ test_that("MTPredict returns correct result for shared features only.", {
   X <- rbind(c(1, 0, -14), c(12, 20, -5))
   Y <- cbind(c(10, -83), c(-9, -53))
 
-  mats <- PrepareMatrices(X = X, Y = Y)
+  mats <- PrepareMatrices(X = X, Y = Y, standardize = FALSE)
   expect_equal(mats$XTX, t(X) %*% X)
   expect_equal(mats$XTY, t(X) %*% Y)
 })
@@ -13,7 +13,7 @@ test_that("MTPredict returns correct result for task specific features only.", {
   tsf <- list(rbind(c(5, 4, 6), c(3, 0, -10)), rbind(c(1, -7, 3), c(1, 5, 0)))
   Y <- cbind(c(10, -83), c(-9, -53))
 
-  mats <- PrepareMatrices(Y = Y, task.specific.features = tsf)
+  mats <- PrepareMatrices(Y = Y, task.specific.features = tsf, standardize = FALSE)
   expect_equal(mats$XTX[[1]], t(tsf[[1]]) %*% tsf[[1]])
   expect_equal(mats$XTX[[2]], t(tsf[[2]]) %*% tsf[[2]])
   expect_equal(mats$XTY[, 1, drop = FALSE], t(tsf[[1]]) %*% Y[, 1])
@@ -25,11 +25,23 @@ test_that("MTPredict returns correct result for mixed features.", {
   tsf <- list(rbind(c(5, 4, 6), c(3, 0, -10)), rbind(c(1, -7, 3), c(1, 5, 0)))
   Y <- cbind(c(10, -83), c(-9, -53))
 
-  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf)
+  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf, standardize = FALSE)
   expect_equal(mats$XTX[[1]], t(cbind(X, tsf[[1]])) %*% cbind(X, tsf[[1]]))
   expect_equal(mats$XTX[[2]], t(cbind(X, tsf[[2]])) %*% cbind(X, tsf[[2]]))
   expect_equal(mats$XTY[, 1, drop = FALSE], t(cbind(X, tsf[[1]])) %*% Y[, 1])
   expect_equal(mats$XTY[, 2, drop = FALSE], t(cbind(X, tsf[[2]])) %*% Y[, 2])
+})
+
+test_that("MTPredict returns correct result for mixed features with standardization.", {
+  X <- rbind(c(1, 0, -14), c(12, 20, -5))
+  tsf <- list(rbind(c(5, 4, 6), c(3, 0, -10)), rbind(c(1, -7, 3), c(1, 5, 0)))
+  Y <- cbind(c(10, -83), c(-9, -53))
+
+  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf)
+  expect_equal(mats$XTX[[1]], t(scale(cbind(X, tsf[[1]]))) %*% scale(cbind(X, tsf[[1]])))
+  expect_equal(mats$XTX[[2]], t(scale(cbind(X, tsf[[2]]))) %*% scale(cbind(X, tsf[[2]])))
+  expect_equal(mats$XTY[, 1, drop = FALSE], t(scale(cbind(X, tsf[[1]]))) %*% scale(Y[, 1]))
+  expect_equal(mats$XTY[, 2, drop = FALSE], t(scale(cbind(X, tsf[[2]]))) %*% scale(Y[, 2]))
 })
 
 test_that("MTPredict returns correct result for mixed features, single task.", {
@@ -37,7 +49,7 @@ test_that("MTPredict returns correct result for mixed features, single task.", {
   tsf <- list(rbind(c(5, 4, 6), c(3, 0, -10)))
   Y <- as.matrix(c(10, -83))
 
-  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf)
+  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf, standardize = FALSE)
   expect_equal(mats$XTX[[1]], t(cbind(X, tsf[[1]])) %*% cbind(X, tsf[[1]]))
   expect_equal(mats$XTY, t(cbind(X, tsf[[1]])) %*% Y)
 })
