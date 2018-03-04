fcb182c806519bfe06fb7cbb3d16f64d59e95fec
tohein <tohein@stud.uni-saarland.de>
2018-03-04 21:01:33 +0100

    updated tests and namespace

--- tests/testthat/test-PrepareMatrices.R
+++ tests/testthat/test-PrepareMatrices.R
@@ -4,7 +4,8 @@ test_that("PrepareMatrices returns correct result for shared features only.", {
   X <- rbind(c(1, 0, -14), c(12, 20, -5))
   Y <- cbind(c(10, -83), c(-9, -53))
 
-  mats <- PrepareMatrices(X = X, Y = Y, standardize = FALSE)
+  mats <- PrepareMatrices(X = X, Y = Y,
+                          standardize = FALSE)
   expect_equal(mats$XTX, t(X) %*% X)
   expect_equal(mats$XTY, t(X) %*% Y)
 })
@@ -13,7 +14,8 @@ test_that("PrepareMatrices returns correct result for task specific features onl
   tsf <- list(rbind(c(5, 4, 6), c(3, 0, -10)), rbind(c(1, -7, 3), c(1, 5, 0)))
   Y <- cbind(c(10, -83), c(-9, -53))
 
-  mats <- PrepareMatrices(Y = Y, task.specific.features = tsf, standardize = FALSE)
+  mats <- PrepareMatrices(Y = Y, task.specific.features = tsf,
+                          standardize = FALSE)
   expect_equal(mats$XTX[[1]], t(tsf[[1]]) %*% tsf[[1]])
   expect_equal(mats$XTX[[2]], t(tsf[[2]]) %*% tsf[[2]])
   expect_equal(mats$XTY[, 1, drop = FALSE], t(tsf[[1]]) %*% Y[, 1])
@@ -25,7 +27,8 @@ test_that("PrepareMatrices returns correct result for mixed features.", {
   tsf <- list(rbind(c(5, 4, 6), c(3, 0, -10)), rbind(c(1, -7, 3), c(1, 5, 0)))
   Y <- cbind(c(10, -83), c(-9, -53))
 
-  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf, standardize = FALSE)
+  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf,
+                          standardize = FALSE)
   expect_equal(mats$XTX[[1]], t(cbind(X, tsf[[1]])) %*% cbind(X, tsf[[1]]))
   expect_equal(mats$XTX[[2]], t(cbind(X, tsf[[2]])) %*% cbind(X, tsf[[2]]))
   expect_equal(mats$XTY[, 1, drop = FALSE], t(cbind(X, tsf[[1]])) %*% Y[, 1])
@@ -49,7 +52,8 @@ test_that("PrepareMatrices returns correct result for mixed features, single tas
   tsf <- list(rbind(c(5, 4, 6), c(3, 0, -10)))
   Y <- as.matrix(c(10, -83))
 
-  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf, standardize = FALSE)
+  mats <- PrepareMatrices(Y = Y, X = X, task.specific.features = tsf,
+                          standardize = FALSE)
   expect_equal(mats$XTX[[1]], t(cbind(X, tsf[[1]])) %*% cbind(X, tsf[[1]]))
   expect_equal(mats$XTY, t(cbind(X, tsf[[1]])) %*% Y)
 })
